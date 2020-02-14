6f2f641cc6205e7b7982f9b3d6b1dc752e55df0f
George Pottanat <morpheus@Georges-MacBook-Pro.local>
2020-02-13 23:01:59 -0500

    first commit

---
+++ Super_App/Super_App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Super_App.Models;
+
+namespace Super_App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
